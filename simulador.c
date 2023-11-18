fc6686c924234a661254ecee3267bc713285edb8
gez_schegtel@tutanota.com <gez_schegtel@tutanota.com>
2023-11-18 11:44:10 -0300

    Se añadió un proceso de carga automática.

--- simulador.c
+++ simulador.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
+#include <time.h>
 
 typedef struct proceso {
     int idProc;
@@ -94,6 +96,63 @@ void cargaManual(void){
         
         insertarOrdenado(&primp, p);
     }
+}
+
+int generacionAleatoria(int min, int max){
+    srand(time(NULL)); /* Hace que la función rand() genere números distintos con el pasar del tiempo. */
+	int numeroAleatorio = min + rand() % max; /* Generamos un número aleatorio entre un mínimo y un máximo solicitado. */
+    sleep(1); /* Dejamos que pase un tiempo para que no se repitan los valores. */
+    return(numeroAleatorio);
+}
+
+void cargaAutomatica(void){
+    /*Lo siguiente es equivalente a la definición de "puntero a Proceso".*/
+    
+    cantProc = controlDeEntradas("número de procesos que desee planificar", 1, 10);
+
+    for (int i = 1; i <= cantProc; i++) {
+        printf("\nGenerando proceso nro. %d \n", i);
+
+        p = (Proceso *)malloc(sizeof(Proceso)); /*De esta manera se crea un nuevo nodo.*/
+        p->idProc = i;
+        p->ta = generacionAleatoria(0, 500);
+        p->ti = generacionAleatoria(1, 500);
+        acuml += p->ti;
+        p->tr = p->ti;
+        p->tam = generacionAleatoria(1, 250);
+        
+        insertarOrdenado(&primp, p);
+    }
+}
+
+void menu(void) {
+    char respuesta[6];
+
+    do {
+        printf("Para cargar manualmente los procesos, ingrese 'manual'. \nSi prefiere generar los procesos de manera automática, ingrese 'auto'. \nSi desea detener la operación, ingrese 'quit'. \n:");
+        scanf("%s", respuesta);
+
+        /* Convertir la respuesta a minúsculas para hacer la comparación no sensible a mayúsculas */
+        for (int i = 0; i < strlen(respuesta); i++) {
+            respuesta[i] = tolower(respuesta[i]);
+        }
+
+        /* Si los dos strings son iguales, la función strcmp devuelve 0 */
+        if (strcmp(respuesta, "manual") == 0) {
+            cargaManual();
+            break;
+        } else if (strcmp(respuesta, "auto") == 0) {
+            cargaAutomatica();
+            break;
+        } else if (strcmp(respuesta, "quit") == 0) {
+            printf("Deteniendo la ejecución...\n");
+            exit(0);
+        } else {
+            printf("Respuesta no válida.\nEjecutando el programa nuevamente... \n");
+            sleep(2);
+            system("clear");
+        }
+    } while(1);
 }
 
 void recorridoListaInicial(Proceso *r){
@@ -222,13 +281,12 @@ void muestrasParciales(Proceso *r){
     }
 }
 
-
 int main(void){
     printf("Este programa es un simulador de asignación de memoria y gestión de procesos. Se permiten hasta 10 procesos con un tamaño de 250 como máximo. \n\n");
 
     iniciarPunterosAuxiliares();
 
-    cargaManual();
+    menu();
 
     recorridoListaInicial(primp);
 
